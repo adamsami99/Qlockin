4786e1f711724aa4b97b28cad8098440be2fa32d
zahraa nahid <zahraanahid66@gmail.com>
2025-02-15 00:12:07 +0100

    making the css for clockIn more attractive and making the support page more interactive

---
+++ scripts/support.js
@@ -0,0 +1,23 @@
+//ensures the JavaScript runs only after the page has fully loaded. 
+document.addEventListener("DOMContentLoaded", function () {
+const searchInput = document.querySelector('.js-search-section');
+const supportBoxes = document.querySelectorAll('.js-support-box');
+
+
+//When user types, the function inside runs automatically.
+searchInput.addEventListener("keyup", function () {
+  const query = searchInput.value.toLowerCase();
+
+  supportBoxes.forEach(box => {
+    const title = box.querySelector('h2').textContent.toLowerCase();
+  //.toLowerCase() makes sure the search is case insensitive (e.g., "ClockIn" = "clockin").
+    const description = box.querySelector('p').textContent.toLowerCase();
+
+    if (title.includes(query) || description.includes(query)) {
+      box.style.display = "block"; 
+    } else {
+      box.style.display = "none";
+      }
+    });
+  });
+});
