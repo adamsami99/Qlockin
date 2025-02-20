0e67f3abb1d7389a093383cf1299b43a943e43e4
Adamsami <adamsami322@gmail.com>
2025-02-20 23:11:50 +0100

    added the clock into weekly site and changed the size on it

--- scripts/monthly.js
+++ scripts/monthly.js
@@ -21,7 +21,6 @@
       const holidays = {
           "2025-01-01": "New Year's Day",
           "2025-12-25": "Christmas Day",
-          "2025-07-04": "Independence Day",
           "2025-11-27": "Thanksgiving"
       };
       
